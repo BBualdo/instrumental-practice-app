b6676f15d0d1326d91afbe03ccf8ccae7ad65a2a
BBualdo <mrseb10@gmail.com>
2026-09-03 23:36:18 +0200

    Added AddRoutineScreen with form and implemented navigation from RoutineListScreen

---
+++ lib/screens/add_routine_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:instrumental/models/instrument.dart';
+import 'package:instrumental/providers/practice_provider.dart';
+import 'package:provider/provider.dart';
+
+class AddRoutineScreen extends StatefulWidget {
+  const AddRoutineScreen({ super.key });
+
+  @override
+  State<AddRoutineScreen> createState() => _AddRoutineScreenState();
+}
+
+class _AddRoutineScreenState extends State<AddRoutineScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _titleController = TextEditingController();
+
+  Instrument _selectedInstrument = Instrument.guitar;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Routine'),
+        actions: [
+          IconButton(
+            onPressed: _saveRoutine, 
+            icon: const Icon(Icons.save)
+            )
+        ]
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                controller: _titleController,
+                decoration: const InputDecoration(
+                  labelText: 'Title',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'Enter routine title';
+                  }
+                  return null;
+                },
+              ),
+
+              const SizedBox(height: 20),
+
+              DropdownButtonFormField<Instrument>(
+                initialValue: _selectedInstrument,
+                decoration: const InputDecoration(
+                  labelText: 'Instrument',
+                  border: OutlineInputBorder()
+                ),
+
+                items: Instrument.values.map((Instrument instrument) {
+                  return DropdownMenuItem<Instrument>(
+                    value: instrument,
+                    child: Text(instrument.name.toUpperCase())
+                  );
+                }).toList(),
+                onChanged: (Instrument? newValue) {
+                  if (newValue != null) {
+                    setState(() {
+                      _selectedInstrument = newValue;
+                    });
+                  }
+                },
+              )
+            ],
+          )
+        )
+      )
+    );
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  void _saveRoutine() {
+    if (_formKey.currentState!.validate()) {
+      final provider = context.read<PracticeProvider>();
+
+      provider.addRoutine(
+        _titleController.text, 
+        _selectedInstrument,
+        []
+      );
+
+      Navigator.of(context).pop();
+    }
+  }
+}
--- lib/screens/routine_list_screen.dart
+++ lib/screens/routine_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instrumental/screens/add_routine_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:instrumental/providers/practice_provider.dart';
 
@@ -42,7 +43,12 @@ class RoutineListScreen extends StatelessWidget {
 
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          print('Add new routine button clicked!');
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const AddRoutineScreen()
+            )
+          );
         },
         child: const Icon(Icons.add)
       ),
